a351a5507f14341903e665b480064e0a99d3a3af
baptiste-pauletto1 <baptistep.contact@gmail.com>
2020-10-08 21:08:03 +0200

    Barre rouge updated

--- scripts/script-inscription.js
+++ scripts/script-inscription.js
@@ -2,14 +2,11 @@ $(function(){
 
     $("#form-inscription").submit(() => {
         if (($("#verif-champ1").val() === "") || ($("#verif-champ2").val() === "") || (!$(".verif-champ3").prop("checked"))){
-            $("#verif-champ1").css("border-width","3px");
-            $("#verif-champ1").css("border-color","red");
+            $("#verif-champ1").css("border","3px solid red");
 
-            $("#verif-champ2").css("border-width","3px");
-            $("#verif-champ2").css("border-color","red");
+            $("#verif-champ2").css("border","3px solid red");
 
-            $(".verif-champ3").css("border-width","3px");
-            $(".verif-champ3").css("border-color","red");
+            $("#verif-champ3-nom").css("border-bottom","3px solid red");
 
             return false;
         } else {

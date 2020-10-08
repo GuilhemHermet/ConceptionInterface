5bcea4c522ba0176a49b9977499b98f0f0b17b5b
baptiste-pauletto1 <baptistep.contact@gmail.com>
2020-10-08 22:40:55 +0200

    typo. once more

--- scripts/script-inscription.js
+++ scripts/script-inscription.js
@@ -30,20 +30,17 @@ $(function(){
             json.password=password;
             console.log(json);
 
-            $('#myModal').modal('show');
-
             let postRequest = new Request("http://127.0.0.1:3000/", {method: 'POST', body: JSON.stringify(json)})
 
             fetch(postRequest)
             .then(response =>{
                 if(response.status === 200){
-                    console.log("coucou");
-                    
+                    console.log("La requête a atteint le serveur");
+                    $('#myModal').modal('show');
                 } else {
                     throw new Error("Failed to send the request");
                 }
-            })
-            
+            })  
         } 
         return false; // permet de ne pas perform l'envoi du form.
     })
